fe420f869bd7fca8d25a9b78a4f703f1d23d32aa
VladislavA966 <afonin30.10.1996@gmail.com>
2023-05-06 19:03:43 +0600

    first commit

---
+++ bin/lesson_3.dart
@@ -0,0 +1,28 @@
+void main() {
+//task 1
+  double b = 4.2;
+  int a = 3;
+  print(a + b);
+
+//task 2
+  String text = 'Name';
+  text = 'Flutter';
+  print('I want to learn $text');
+
+//task 3
+  double myNumber = 2.225;
+  String myText;
+  myText = myNumber.toString();
+  print(myText);
+
+//task 4
+  String z = '10';
+  String x = '12';
+
+  print(int.parse(z) + int.parse(x));
+
+//task 5
+  int c = 10;
+  int v = 12;
+  double n = c / v;
+}
